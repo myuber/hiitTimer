4e4a70ee6d39c804f183544042a7361d8540323d
myuber <ogari.gari.za@gmail.com>
2020-08-07 23:01:37 +0900

    Timerページに使用するために、一旦ストップウォッチ機能を実装しました

--- HiitTimer/HiitTimer/TimerView.swift
+++ HiitTimer/HiitTimer/TimerView.swift
@@ -7,10 +7,65 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct TimerView: View {
+    @ObservedObject var countdowntimer = CountDownTimer()
+    
     var body: some View {
-        Text("Timerページ")
+        VStack {
+            HStack {
+                // スタートボタン
+                Button(action: {
+                    self.countdowntimer.start()
+                }) {
+                    Image(systemName: "play")
+                }.padding()
+                
+                // 一時停止ボタン
+                Button(action: {
+                    self.countdowntimer.stop()
+                }) {
+                    Image(systemName: "pause")
+                }.padding()
+                
+                // 中断ボタン
+                Button(action: {
+                    self.countdowntimer.reset()
+                }) {
+                    Image(systemName: "backward.end")
+                }.padding()
+            }
+            .frame(width: 200)
+            // タイマーを表示
+            Text("\(self.countdowntimer.counter)")
+        }.font(.largeTitle)
+    }
+}
+
+// タイマークラスを定義
+class CountDownTimer: ObservableObject {
+    // 共有できる変数
+    @Published var counter: Int = 0
+    // タイマーを作成
+    var timer = Timer()
+    
+    // 実際に使うタイマーを作ってスタート
+    func start() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {_ in
+            self.counter += 1
+        }
+    }
+    
+    // タイマーを止める
+    func stop() {
+        timer.invalidate()
+    }
+    
+    // タイマーをリセット
+    func reset() {
+        timer.invalidate()
+        counter = 0
     }
 }
 
