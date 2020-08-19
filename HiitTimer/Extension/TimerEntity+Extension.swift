045f2facfbd27b9d74dbf0fd7478363436ea61e8
myuber <ogari.gari.za@gmail.com>
2020-08-20 08:22:18 +0900

    SettingViewの修復が完了した

---
+++ HiitTimer/Extension/TimerEntity+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  TimerEntity+Extension.swift
+//  HiitTimer
+//
+//  Created by がり on 2020/08/19.
+//  Copyright © 2020 がり. All rights reserved.
+//
+
+import CoreData
+import SwiftUI
+
+extension TimerEntity{
+    
+    /*static func counts(in managedObjectContext: NSManagedObjectContext,
+                       category: Category,
+                       task: String,
+                       time: Date? = Date()){
+        let todo = self.init(context: managedObjectContext)
+        print(task)
+        todo.time = time
+        todo.category = category.rawValue
+        todo.task = task
+        todo.state = State.todo.rawValue
+        
+        do {
+            try  managedObjectContext.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+ */
+}
---
+++ HiitTimer/HiitTimer/TimerView.swift
@@ -0,0 +1,79 @@
+//
+//  TimerView.swift
+//  HiitTimer
+//
+//  Created by がり on 2020/08/19.
+//  Copyright © 2020 がり. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var isTimer: Bool = true
+    @State var isModal = false
+    
+    var body: some View {
+        Text("Hello world")
+        /*VStack {
+            Text(isTimer ? "Training" : "Interval")
+            
+            // タイマーを表示
+            ZStack {
+                // 数字をタップするとモーダルビューが表示される
+                VStack {
+                    Button(action: {
+                        self.isModal = true
+                    }) {
+                        Text("\(self.countdowntimer.counter)")
+                    }
+                    Button(action: {
+                        self.isModal = true
+                    }) {
+                        Text("\(self.intervaltimer.counter)")
+                    }
+                } // シートで数値を変えるごとに共有データShareDataのTIMES配列から数値を持ってきて、タイマーの数値を変更する
+            .sheet(isPresented: $isModal,
+                   onDismiss:{}
+                ){SettingView().environmentObject(self.shareData)} // モーダルビューに共有データを教える
+
+            // 背景のグレーの円を描画
+            Circle()
+                .stroke(Color(.systemGray), style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .bevel))
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            // 水色のプログレスバーを描画
+            Circle()
+                .trim(from: 0, to: isTimer ?
+                    (CGFloat(self.countdowntimer.counter)/CGFloat(self.shareData.TIMES[self.shareData.selectTime])) :
+                    (CGFloat(self.intervaltimer.counter)/CGFloat(self.shareData.INTERVALS[self.shareData.selectInterval]))
+                ).stroke(Color(.systemTeal), style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .bevel))
+                .aspectRatio(contentMode: .fit)
+                .rotationEffect(Angle(degrees: -90))
+                .padding()
+        }
+            
+        HStack {
+            // スタートボタン
+            Button(action: {}) {
+                Image(systemName: "play")
+            }.padding()
+            
+            // 一時停止ボタン
+            Button(action: {}) {
+                Image(systemName: "pause")
+            }.padding()
+            
+            // 中断ボタン
+            Button(action: {}) {Image(systemName: "backward.end")}.padding()
+        }.frame(width: 200)
+        */
+    } // body
+} // struct
+
+    
+    
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
