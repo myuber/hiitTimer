13fd7466c9f5e83bc2430fa0c1f5f06d4e147d28
myuber <ogari.gari.za@gmail.com>
2020-08-27 08:26:45 +0900

    ステータス入力View完成。ShareDataに値を渡せるように変更する

--- HiitTimer_pre/HiitTimer/CalcInputView.swift
+++ HiitTimer_pre/HiitTimer/CalcInputView.swift
@@ -12,46 +12,64 @@ struct CalcInputView: View {
     // 共有のデータを保持する
     @EnvironmentObject var shareData: ShareData
     
+    @State var weight:String = ""
+    @State var height:String = ""
+    
+    var EXSERCISEPATTERN = [
+        "普段は全く運動をしていない",
+        "週1〜2回程度、運動している",
+        "週2〜3回程度、運動している",
+        "週3〜4回程度、運動している",
+        "ほぼ毎日激しい運動をしている"
+    ]
+    
     var body: some View {
-        Form{
-            // 年齢
-            Section(header: Text("年齢を入力してください")){
-                Picker(selection: $shareData.userAge, label: Text("年齢")) {
-                    ForEach(0..<100) { index in
-                        Text("\(index)歳").tag(index)
+        NavigationView{
+            Form{
+                // 年齢
+                Section(header: Text("年齢を選択してください")){
+                    Picker(selection: $shareData.userAge, label: Text("年齢")) {
+                        ForEach(0..<100) { index in
+                            Text("\(index)歳").tag(index)
+                        }
                     }
                 }
-            }
-            // 性別
-            Section(header: Text("性別を入力してください")){
-                Picker(selection: $shareData.userGender, label: Text("性別")) {
-                    Text("女性").tag("female")
-                    Text("男性").tag("male")
-                }
-            }
-            
-            // 体重と身長
-            Section(header: Text("体重と年齢を入力してください")){
-                Picker(selection: $shareData.userWeight, label: Text("体重")) {
-                    ForEach(30..<150) { index in
-                        Text("\(index)kg").tag(index)
+                // 性別
+                Section(header: Text("性別を選択してください")){
+                    Picker(selection: $shareData.userGender, label: Text("性別")) {
+                        Text("女性").tag("female")
+                        Text("男性").tag("male")
                     }
                 }
-                Picker(selection: $shareData.userHeight, label: Text("身長")) {
-                    ForEach(130..<200) { index in
-                        Text("\(index)cm").tag(index)
+                
+                // 体重と身長
+                Section(header: Text("体重と年齢を入力してください")){
+                    
+                    HStack {
+                        TextField("体重(kg)",text: $weight).keyboardType(.numberPad)
+                        Text("kg")
+                            .foregroundColor(Color.gray)
+                    }
+                    HStack {
+                        TextField("身長(cm)",text: $height).keyboardType(.numberPad)
+                        Text("cm")
+                            .foregroundColor(Color.gray)
                     }
                 }
-            }
-            
-            // 運動強度
-            Section(header: Text("1週間の運動量を入力してください")){
-                Picker(selection: $shareData.userAge, label: Text("運動レベル")) {
-                    ForEach(0..<100) { index in
-                        Text("\(index)歳").tag(index)
+                
+                // 運動強度
+                Section(header: Text("1週間の運動量を選択してください")){
+                    Picker(selection: $shareData.userAge, label: Text("運動レベル")) {
+                        ForEach(0 ..< EXSERCISEPATTERN.count) { index in
+                            Text("\(self.EXSERCISEPATTERN[index])").tag(index)
+                        }
                     }
                 }
-            }
+            }.navigationBarItems(trailing: Button(action: {
+                
+            }) {
+                Text("TDEEを計算する")
+            })
         }
     }
 }
--- HiitTimer_pre/HiitTimer/CalcView.swift
+++ HiitTimer_pre/HiitTimer/CalcView.swift
@@ -9,12 +9,19 @@
 import SwiftUI
 
 struct CalcView: View {
+    // 共有のデータを保持する
+    @EnvironmentObject var shareData: ShareData
+    
     var body: some View {
         NavigationView{
             VStack{
                 NavigationLink(destination: CalcInputView()){
                     Text("TDEE計算ページ")
                 }
+                Text("年齢\(self.shareData.userAge)")
+                Text("性別\(self.shareData.userGender)")
+                Text("体重\(self.shareData.userWeight)")
+                Text("身長\(self.shareData.userHeight)")
             }
         }
     }
